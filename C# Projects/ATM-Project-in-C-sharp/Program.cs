70fff187e2c9e7637d82637ef3c6d4a6209acc7a
Alberto Alvarado Jr <65637580+albertgs09@users.noreply.github.com>
2022-10-17 14:22:19 -0500

    C-Sharp

---
+++ C# Projects/ATM-Project-in-C-sharp/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+// ATM program
+public class cardHolder
+{
+    String cardNum;
+    int pin;
+    String firstName;
+    String lastName;
+    double balance;
+
+    public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
+    {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    public String getNum()
+    {
+        return cardNum;
+    }
+
+    public int getPin()
+    {
+        return pin;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setNum(String newCardNum)
+    {
+        cardNum = newCardNum;
+    }
+
+    public void setPin(int newPin)
+    {
+        pin = newPin;
+    }
+
+    public void setFirstName(String newFirstName)
+    {
+        firstName = newFirstName;
+    }
+    public void setLastName(String newLastName)
+    {
+        lastName = newLastName;
+    }
+
+    public void setBalance(double newBalance)
+    {
+        balance = newBalance;
+    }
+
+    public static void Main(String[] args)
+    {
+        void printOptions()
+        {
+
+            Console.WriteLine("Please choose from one of the following options...");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+        }
+
+        void deposit(cardHolder currentUser)
+        {
+            Console.WriteLine("How much $ would you like to deposit?");
+            double deposit = Double.Parse(Console.ReadLine());
+            currentUser.setBalance(currentUser.getBalance() + deposit);
+            Console.WriteLine("Thank you for you $$. Your new balance is: " + currentUser.getBalance());
+        }
+
+        void withdraw(cardHolder currentUser)
+        {
+            Console.WriteLine("How much $ would you like to withdraw: ");
+            double withdrawal = Double.Parse(Console.ReadLine());
+
+            //Check if the user has enough money
+            if(currentUser.getBalance() < withdrawal)
+            {
+                Console.WriteLine("Insufficient balance :(");
+            }
+            else
+            {
+                currentUser.setBalance(currentUser.getBalance() - withdrawal);
+                Console.WriteLine("You're good to go! Thank you :)");
+            }
+
+        }
+
+        void balance(cardHolder currentUser)
+        {
+            Console.WriteLine("Current balance: " + currentUser.getBalance());
+        }
+
+        List<cardHolder> cardHolders = new List<cardHolder>();
+        cardHolders.Add(new cardHolder("1234567812345678", 1234, "Albert", "Alvarado", 150.32));
+        cardHolders.Add(new cardHolder("2234567812345678", 4321, "Joc", "Cav", 2444.32));
+        cardHolders.Add(new cardHolder("3234567812345678", 1342, "Esai", "Fonseca", 15220.32));
+        cardHolders.Add(new cardHolder("4234567812345678", 2341, "Bridger", "Cock", 444432.23));
+        cardHolders.Add(new cardHolder("5234567812345678", 3214, "Aidn", "Alvarado", 0));
+
+        //Prompt user
+        Console.WriteLine("Welcome to MyATM");
+        Console.WriteLine("Please insert your debit card");
+        String debitCardNum = "";
+        cardHolder currentUser;
+
+        while (true)
+        {
+            try
+            {
+                debitCardNum = Console.ReadLine();
+                //Check against our db
+                currentUser = cardHolders.FirstOrDefault(a => a.cardNum == debitCardNum);
+                if(currentUser != null) { break; }
+                else
+                {
+                    Console.WriteLine("Card not recognized. Please try again");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Card not recognized. Please try again");
+            }
+        }
+        Console.WriteLine("Please enter your pin: ");
+        int userPin = 0;
+
+        while (true)
+        {
+            try
+            {
+                userPin = int.Parse(Console.ReadLine());
+                if (currentUser.getPin() == userPin) { break; }
+                else
+                {
+                    Console.WriteLine("Incorrect pin. Please try again");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Incorrect pin. Please try again");
+            }
+        }
+        Console.WriteLine("Welcome " + currentUser.getFirstName() + " :)");
+        int option = 0;
+        do
+        {
+            printOptions();
+            try
+            {
+                option = int.Parse(Console.ReadLine());
+            }
+            catch { }
+            if(option == 1) { deposit(currentUser); }
+            else if(option == 2) { withdraw(currentUser); }
+            else if(option == 3) { balance(currentUser); }
+            else if ( option == 4 ) { break; }
+            else { option = 0; }
+            
+        }
+        while (option != 4);
+        Console.WriteLine("Thank you! Have a nice day :)");
+    }
+}
---
+++ C# Projects/ATM-Project-in-C-sharp/README.md
@@ -0,0 +1,4 @@
+# ATM-Project-in-C-sharp
+
+Simple ATM project, acts as a ATM. You can check the balance of users in the "database" , deposit and withdraw money. Makes sure the name, card number and pin match up before showing 
+you any information.
